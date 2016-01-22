dae0c3b542f4199d81f08eeb4b28a9a6020ff120
Steven Buxton <breathesteve@gmail.com>
2016-01-22 15:02:48 -0600

    set up api php reference

---
+++ twitter.php
@@ -0,0 +1,17 @@
+<?php
+	session_start();
+
+	require_once('src/TwitterOAuth.php');
+
+	$apiKey ="5GeRCyt189iIK6s1c9n00lNlH";
+	$apiSecret = "jy1fpyjcGC061VOutnEOrqacuoA5Hi9Md6Y6tNgS63U4ZIoB1s";
+	$accessToken = "4788557236-x2nSWa7C8nz1lvYi4WJfprmsge444sXXHANTxv5";
+	$accessSecret = "07IYM885FyBnabZkKGuE1frZCzeKyLHeAhj6oVyVU6I2Q";
+
+	$connection = new TwitterOAuth($apiKey, $apiSecret, $accessToken, $accessSecret);
+
+	$tweets = $connection->get('https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=twitterapi&count=2');
+
+	print_r($tweets);
+
+?>
